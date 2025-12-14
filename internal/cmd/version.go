622dd98bead5726b415e0a4f91b57213ff4da35c
José Carlos Chávez <jcchavezs@gmail.com>
2025-12-14 22:17:10 +0100

    feat: adds support for version

---
+++ internal/cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// version is set at build time via ldflags
+var version = "dev"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of chasky",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Fprintf(cmd.OutOrStdout(), "chasky %s\n", version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
